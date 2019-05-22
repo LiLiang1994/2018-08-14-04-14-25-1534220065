87158f52f04105db22905570d8201322ccd9786a
Li1994 <309820720@qq.com>
2019-05-22 17:13:18 +0800

    update main

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(input) {
+	var result="";
+	var nums=new Map([['0',["._.","|.|","|_|"]],['1',["...","..|","..|"]],['2',["._.","._|","|_."]],['3',["._.","._|","._|"]],['4',["...","|_|","..|"]],['5',["._.","|_.","._|"]],['6',["._.","|_.","|_|"]],['7',["._.","..|","..|"]],['8',["._.","|_|","|_|"]],['9',["._.","|_|","..|"]]]);
+	
+	for(var j=0;j<3;j++){
+		for(var i=0;i<input.length;i++){
+			result+=nums.get(input.charAt(i))[j];
+			if(i<input.length-1){
+				result+=" ";
+			}
+		}
+		result+="\n";
+	}
+	
+	console.log("result");
+    console.log(result);
+    return result;
 };
